44346d201b3b16c5fa45743cdd05977c71dae319
ed00m:th3d00m <github@incubaideas.org>
2012-12-23 02:25:54 -0300

    Config file with basics inputs

---
+++ __config/openDioniso_config.php
@@ -0,0 +1,31 @@
+<?php
+
+    /*
+     * openDioniso - MARKETING
+     * CONFIG file
+     *
+     * Developed by Pablo Zuniga | ed00m@gnulinuxporqueno.cl
+     * Sat Dec 23 02:10:05 CLST 2012
+     * 
+     */
+
+/*
+ * BUSINESS
+ */
+
+$companyName = "";
+$companyMail = "";
+
+
+/*
+ * REPORTS
+ */
+
+$mailRecipientReports = "";
+$mailSenderReports = "";
+$caseBirthday = $companyName."";
+
+
+
+
+?>
